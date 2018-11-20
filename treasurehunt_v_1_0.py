766bdbd7f3c8dac70f8de2d76f130f5822458d0a
madhuvalan <madhu.valan@gmail.com>
2018-11-20 10:17:44 +0530

    Adding first version of treasurehunt

---
+++ treasurehunt_v_1_0.py
@@ -0,0 +1,117 @@
+import os
+import time
+import random
+
+
+def userScreen(*args):
+   
+	os.system("clear")
+	gatesList = list(args)
+	print('DEBUG {}'.format(gatesList))
+
+	print('Hello User !\n')
+	print('\t\t\t\t\t\t\t\t\tTreasure\n') 
+        gates = 'Gate\t\tGate2\t\tGate3\t\tGate4\t\tGate5\t\tGate6\t\tGate7\n' 
+	print(gates)
+       
+	print('\n')
+        list1 =[]
+        entry1 = random.randint(1,7)
+        list1.append(entry1)
+        while len(list1) < 4:
+          entry2 = random.randint(1,7)
+          entry3 = random.randint(1,7) 
+          if entry2 not in list1 and entry3 not in list1:
+              list1.append(entry2)
+              list1.append(entry3)
+          else:
+              entry2 = random.randint(1,7)
+              entry3 = random.randint(1,7)      
+            
+        #entry3 = random.randint(1,7)       
+        #entry1 = int(input('Choice1 !\n'))
+        #entry2 = int(input('Choice2 !\n'))
+        #entry3 = int(input('Choice3 !\n'))    
+
+        userScreen2(entry1,entry2,entry3)             
+#############################################################################################################################################################################################################   
+def userScreen2(*args):
+        count = 0
+        gate1 = "Gate1"
+	gate2 = "Gate2"
+	gate3 = "Gate3"
+        gate4 = "Gate4"   
+      	gate5 = "Gate5"   
+        gate6 = "Gate6"   
+        gate7 = "Gate7"   
+	os.system("clear")
+	gatesList = list(args)
+        #print(type(gatesList))
+	#print('DEBUG {}'.format(gatesList))
+	print('\t\t\t\t\t\t\t\t\tTreasure\n') 
+     
+	for item in gatesList:
+           if item == 1:
+	   	   gate1 = 'Gate1-Guarded'
+	 
+	   elif item == 2:
+	   	   gate2 = 'Gate2-Guarded'
+	   	 
+           elif item == 3:
+	   	   gate3 = 'Gate3-Guarded'
+      
+           elif item == 4:
+	   	   gate4 = 'Gate4-Guarded'
+             
+	   elif item == 5:
+	   	   gate5 = 'Gate5-Guarded'
+           elif item == 6:
+	   	   gate6 = 'Gate6-Guarded'
+           elif item == 7:
+	   	   gate7 = 'Gate7-Guarded'
+     	   else:
+	 	  print('test') 
+              
+        gates = gate1+'\t\t'+gate2+'\t\t'+gate3+'\t\t'+gate4+'\t\t'+gate5+'\t\t'+gate6+'\t\t'+gate7
+        print(gates)
+        print('\n') 
+
+
+       	ele1 = int(input('Choice1 !\n'))
+        ele2 = int(input('Choice2 !\n'))
+        ele3 = int(input('Choice3 !\n'))  
+        userEntry = [ele1,ele2,ele3]
+        print("DEBUG userEntry {}".format(userEntry))
+        randGates = [random.randint(1,7),random.randint(1,7),random.randint(1,7)]        
+        print("DEBUG randGates {}".format(randGates))
+        for i in randGates:
+             print('Gate-{} Guarded'.format(i))    
+        intList = list(set(randGates)& set(userEntry))
+     
+ 
+        print('intList {}'.format(intList))  
+        print('length intList {}'.format(len(intList)))     
+        if len(intList) == 2:
+           print('Sorry Better luck next time, you need to get atleast 2 gates correct ')    
+        else:
+           print('Congratulations! you got the treasure')    
+                   
+        
+        
+
+#############################################################################################################################################################################################################
+list1 =[]
+entry1 = random.randint(1,7)
+list1.append(entry1)
+while len(list1) < 4:
+     entry2 = random.randint(1,7)
+     entry3 = random.randint(1,7) 
+     if entry2 not in list1 and entry3 not in list1:
+           list1.append(entry2)
+           list1.append(entry3)
+     else:
+           entry2 = random.randint(1,7)
+           entry3 = random.randint(1,7)      
+            
+userScreen2(entry1,entry2,entry3)  
+
